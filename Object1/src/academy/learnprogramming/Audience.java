95c8ef8083c389805728b94789557c28ca5f596a
leeheeweon <ggoddii@kaoni.com>
2021-10-12 09:41:31 +0900

    Object1

---
+++ Object1/src/academy/learnprogramming/Audience.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
---
+++ Object1/src/academy/learnprogramming/TicketSeller.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+    
+}
