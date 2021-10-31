14842286de651d9f86b6199eb744d48118ecf1d0
leeheeweon <ggoddii@kaoni.com>
2021-11-01 08:45:19 +0900

    오브젝트 164P

--- MovieTicketTradeSystemData/src/academy/learnprogramming/Movie.java
+++ MovieTicketTradeSystemData/src/academy/learnprogramming/Movie.java
@@ -3,42 +3,20 @@ package academy.learnprogramming;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
 
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
-
-
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case AMOUNT_DISCOUNT:
-                return calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-            case NONE_DISCOUNT:
-                return calculateNoneDiscountAmount();
-        }
-
-        throw new IllegalStateException();
-    }
-
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = Arrays.asList(discountConditions);
     }
 
     public Money calculateMovieFee(Screening screening) {
@@ -49,8 +27,15 @@ public class Movie {
         return fee;
     }
 
-    private boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
-        return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(sequence));
+    private boolean isDiscountable(Screening screening) {
+        return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    abstract protected Money calculateDiscountAmount();
+
+
+    protected Money getFee() {
+        return fee;
     }
 }
 
---
+++ MovieTicketTradeSystemData/src/academy/learnprogramming/NoneDiscountMovie.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+import java.time.Duration;
+
+public class NoneDiscountMovie extends Movie {
+    public NoneDiscountMovie(String title, Duration runningTime, Money fee) {
+        super(title, runningTime, fee);
+    }
+
+    @Override
+    protected Money calculateDiscountAmount() {
+        return Money.ZERO;
+    }
+}
